78ce8fda5b939fb4ef4de6586a552c18d1f919c0
JvPelai <jv.pelai@gmail.com>
2021-07-20 13:42:37 -0300

    db associations, crud methods and routes

--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -8,5 +8,9 @@ router.get('/pessoas/:id', PessoaController.getPessoa);
 router.post('/pessoas', PessoaController.createPessoa);
 router.put('/pessoas/:id', PessoaController.updatePessoa);
 router.delete('/pessoas/:id', PessoaController.deletePessoa);
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criaMatricula)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
 
 module.exports = router;
