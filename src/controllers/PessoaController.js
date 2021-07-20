661092be6431e6d50fd0c516daa807c69c173a7a
JvPelai <jv.pelai@gmail.com>
2021-07-20 12:38:00 -0300

    crud

--- src/controllers/PessoaController.js
+++ src/controllers/PessoaController.js
@@ -1,13 +1,56 @@
 const database = require('../models');
 
 class PessoaController {
-    static async listaPessoas (req, res) {
+    static async listaPessoas(req, res) {
         try {
             const pessoasList = await database.Pessoas.findAll();
             return res.status(200).json(pessoasList)
         } catch (error) {
             return res.status(500).json(error.message);
         }
+    }
+
+    static async getPessoa(req, res) {
+        const { id } = req.params;
+        try {
+            const pessoa = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(pessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async createPessoa(req, res) {
+        const novaPessoa = req.body;
+        try {
+            const createdPessoa = await database.Pessoas.create(novaPessoa);
+            return res.status(200).json(createdPessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async updatePessoa(req, res) {
+        const { id } = req.params;
+        const newInfos = req.body;
+        try {
+            await database.Pessoas.update(newInfos, { where: { id: Number(id) } })
+            const updatedPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } })
+            return res.status(200).json(updatedPessoa);
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
+    }
+
+    static async deletePessoa(req, res) {
+        const { id } = req.params;
+        try {
+            await database.Pessoas.destroy({ where: { id: Number(id) } })
+            return res.status(200).json({ message: `id ${id} deletado` })
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+
     }
 }
 
--- src/routes/pessoasRoute.js
+++ src/routes/pessoasRoute.js
@@ -4,5 +4,9 @@ const PessoaController = require('../controllers/PessoaController')
 const router = Router()
 
 router.get('/pessoas', PessoaController.listaPessoas);
+router.get('/pessoas/:id', PessoaController.getPessoa);
+router.post('/pessoas', PessoaController.createPessoa);
+router.put('/pessoas/:id', PessoaController.updatePessoa);
+router.delete('/pessoas/:id', PessoaController.deletePessoa);
 
 module.exports = router;
